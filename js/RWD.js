405696bde06d631e08e73d465e6bbc611b7e8f18
Ivan_Loichuk <ivan.loichuk@pollub.edu.pl>
2017-08-10 11:21:35 +0200

    start build profile page

---
+++ js/RWD.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Vania-PC on 08.08.2017.
+ */
+$(document).ready(function () {
+   if($( window ).width() <= 480){
+
+   }
+
+
+
+
+});
+if (screen.width >= 600) {
+    // download complicated script
+    // swap in full-source images for low-source ones
+}
+
+if (document.documentElement.clientWidth < 900) {
+    // scripts
+}
