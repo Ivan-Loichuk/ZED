d4a6107ae522ab3c738d914d059f605aec246ca9
Ivan_Loichuk <ivan.loichuk@pollub.edu.pl>
2017-08-23 19:33:57 +0200

    friends page (all)

--- js/main.js
+++ js/main.js
@@ -23,7 +23,7 @@ $(document).ready(function () {
         if (parseInt($('#w').val())) return true;
         alert('Please select a crop region then press submit.');
         return false;
-    };
+    }
 
     /* end crop functions */
 
@@ -42,11 +42,50 @@ $(document).ready(function () {
 
 /* links hover effect*/
     // exchange image when mouse hover
-    $(link_edit_icon).hover(function(){
-            $('#edit_icon').attr('src', '../img/edit_icon_black.png');
-        },
-        function(){
-            $('#edit_icon').attr('src', '../img/edit_icon_gray.png');
+    if($('#link_edit_icon').length == 1) {
+        $('#link_edit_icon').hover(function () {
+                $('#edit_icon').attr('src', '../img/edit_icon_black.png');
+            },
+            function () {
+                $('#edit_icon').attr('src', '../img/edit_icon_gray.png');
+            });
+    }
+    // some effects when link is active (search page)
+    if($('#search_menu').length == 1) {
+        $('#search_menu li > a').click(function () {
+            $('#search_menu a').removeClass();
+            $(this).addClass('active');
         });
+    }
+/* generate age value for search select */
+    if($('#age_from').length == 1 || $('#age_to').length == 1) {
+        for (var i = 0; i < 100; i++) {
+            $('#age_from').append($('<option> </option>').val(i).html(i));
+        }
+        for (var j = 0; j < 100; j++) {
+            $('#age_to').append($('<option> </option>').val(j).html(j));
+        }
+    }
+/* end generate value */
+
+/* friends page left navbar */
+    if($('#nav-friends').length == 1) {
+        $('#nav-friends li').click(function () {
+            $('#nav-friends li').removeClass();
+            $(this).addClass('active');
+            if($(this).children()[0].className == "myFriends"){
+               $('.followers').css('display', 'none');
+               $('.online_group').css('display', 'block');
+               $('.nav-friends').css('display', 'inline-block');
+               $('.nav-followers').css('display', 'none');
+            }
+            if($(this).children()[0].className == "myFollowers"){
+                $('.online_group').css('display', 'none');
+                $('.followers').css('display', 'block');
+                $('.nav-friends').css('display', 'none');
+                $('.nav-followers').css('display', 'inline-block');
+            }
+        });
+    }
 
 });
